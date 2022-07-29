3a7db39890b1142ad4cdec5e72bffc8c8bfcc9c8
Curly <niklas.schmidt@traperto.com>
2022-07-29 12:07:48 +0200

    feat: Update + code cleanup
    - Updated project
      - target framework
      - dependencies
    - code cleanup
      - nullable enabled (prevents System.NullReferenceException on malformed requests)
      - ClamClient should not be initialized in controller
      - enforced internal code conventions
    
    TES-1871

--- Traperto.ClamAvRest.Main/Controllers/ScanController.cs
+++ Traperto.ClamAvRest.Main/Controllers/ScanController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Net;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,67 +11,35 @@ namespace VirusScannerService.Controllers
     [Route("[controller]")]
     public class ScanController : ControllerBase
     {
-
-        private const string SuccessResultText = "Everything ok : true";
-        private const string InfectionsResultText = "Everything ok : false";
+        private const string _SUCCESS_RESULT_TEXT = "Everything ok : true";
+        private const string _INFECTIONS_RESULT_TEXT = "Everything ok : false";
 
         private readonly ILogger<ScanController> _logger;
+        private readonly ClamClient _clamClient;
 
         public ScanController(
-            ILogger<ScanController> logger
+            ILogger<ScanController> logger,
+            ClamClient clamClient
         )
         {
             _logger = logger;
+            _clamClient = clamClient;
         }
 
         [HttpPost]
-        public async Task<OkObjectResult> Scan(IFormFile file)
+        public async Task<OkObjectResult> Scan(IFormFile file, CancellationToken cancellationToken)
         {
-            var port = int.Parse(Environment.GetEnvironmentVariable("PORT") ?? "3310");
-            var host = Environment.GetEnvironmentVariable("HOST");
-            var ip = Environment.GetEnvironmentVariable("IP");
-            var maxStreamSize = Environment.GetEnvironmentVariable("MAX_STREAM_SIZE");
-            var maxChunkSize = Environment.GetEnvironmentVariable("MAX_CHUNK_SIZE");
-
-            ClamClient clam;
-
-            if (ip != null)
-            {
-                clam = new ClamClient(IPAddress.Parse(ip), port);
-            }
-            else if (host != null)
-            {
-                clam = new ClamClient(host, port);
-            }
-            else
-            {
-                throw new Exception("no valid configuration");
-            }
-
-            if (maxStreamSize != null)
-            {
-                clam.MaxStreamSize = int.Parse(maxStreamSize); 
-            }
-            
-            if (maxChunkSize != null)
-            {
-                clam.MaxChunkSize = int.Parse(maxChunkSize); 
-            }
-
             var stream = file.OpenReadStream();
-            var scanResult = await clam.SendAndScanFileAsync(stream);
+            var scanResult = await _clamClient.SendAndScanFileAsync(stream, cancellationToken);
 
-            if (scanResult.InfectedFiles != null && scanResult.InfectedFiles.Count > 0)
+            if (scanResult.InfectedFiles is { Count: > 0 })
             {
-                
-                _logger.LogWarning($"File {file.FileName} infected");
-                
-                return Ok(InfectionsResultText);
+                _logger.LogWarning("File {fileName} infected", file.FileName);
+                return Ok(_INFECTIONS_RESULT_TEXT);
             }
-            
-            _logger.LogInformation($"File {file.FileName} ok");
 
-            return Ok(SuccessResultText);
+            _logger.LogInformation("File {fileName} ok", file.FileName);
+            return Ok(_SUCCESS_RESULT_TEXT);
         }
     }
 }
--- Traperto.ClamAvRest.Main/Startup.cs
+++ Traperto.ClamAvRest.Main/Startup.cs
@@ -1,9 +1,12 @@
+using System;
+using System.Net;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using nClam;
 
 namespace VirusScannerService
 {
@@ -13,12 +16,9 @@ namespace VirusScannerService
         {
             var builder = new ConfigurationBuilder()
                 .AddEnvironmentVariables();
-            Configuration = builder.Build();
+            builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -28,14 +28,48 @@ namespace VirusScannerService
                 fo.ValueLengthLimit = int.MaxValue;
                 fo.MultipartBodyLengthLimit = int.MaxValue; // In case of multipart
             });
+
+            services.AddScoped(_ =>
+            {
+                var port = int.Parse(Environment.GetEnvironmentVariable("PORT") ?? "3310");
+                var host = Environment.GetEnvironmentVariable("HOST");
+                var ip = Environment.GetEnvironmentVariable("IP");
+                var maxStreamSize = Environment.GetEnvironmentVariable("MAX_STREAM_SIZE");
+                var maxChunkSize = Environment.GetEnvironmentVariable("MAX_CHUNK_SIZE");
+
+                ClamClient clamClient;
+
+                if (ip != null)
+                {
+                    clamClient = new ClamClient(IPAddress.Parse(ip), port);
+                }
+                else if (host != null)
+                {
+                    clamClient = new ClamClient(host, port);
+                }
+                else
+                {
+                    throw new Exception("no valid configuration");
+                }
+
+                if (maxStreamSize != null)
+                {
+                    clamClient.MaxStreamSize = int.Parse(maxStreamSize); 
+                }
+            
+                if (maxChunkSize != null)
+                {
+                    clamClient.MaxChunkSize = int.Parse(maxChunkSize); 
+                }
+
+                return clamClient;
+            });
+            
             services.Configure<KestrelServerOptions>(options => options.Limits.MaxRequestBodySize = int.MaxValue);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-//            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
